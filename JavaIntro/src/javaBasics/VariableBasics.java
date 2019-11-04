e74f8aacdde5fe228cb04a3f2b1a15fb71711879
chiemerie.noble <chiemerie.noble@AMS-GYM-EDV1-05.gym.ams.local>
2019-11-04 18:52:05 +0100

    basics

--- JavaIntro/src/javaBasics/VariableBasics.java
+++ JavaIntro/src/javaBasics/VariableBasics.java
@@ -3,7 +3,34 @@ package javaBasics;
 public class VariableBasics {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// wahr/falsch
+		// boolean 1 bit
+		boolean wahr_oder_falsch;
+		//
+		wahr_oder_falsch = true;
+		//
+		wahr_oder_falsch = false;
+		System.out.println(wahr_oder_falsch);
+		
+		// byte
+		byte zahl = 1;
+		System.out.println(zahl);
+		System.out.println(Short.MIN_VALUE);
+		System.out.println(Short.MAX_VALUE);
+		
+		// short - 2byte
+		short ganz_kleine_zahl = 120;
+		System.out.println(ganz_kleine_zahl);
+		System.out.println(Short.MIN_VALUE);
+		System.out.println(Short.MAX_VALUE);
+		
+		// INT - 4 byte
+		int kleine_zahl = 123456;
+		System.out.println(kleine_zahl);
+		System.out.println(Integer.MIN_VALUE);
+		System.out.println(Integer.MAX_VALUE);
+		
+		
 
 	}
 
