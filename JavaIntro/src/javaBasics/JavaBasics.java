4572e4d1cd8cf175e82f242bdeb6d3f2eab07758
chiemerie.noble <chiemerie.noble@AMS-GYM-EDV1-05.gym.ams.local>
2019-11-04 18:51:33 +0100

    basic

--- JavaIntro/src/javaBasics/JavaBasics.java
+++ JavaIntro/src/javaBasics/JavaBasics.java
@@ -34,6 +34,20 @@ public class JavaBasics {
 		System.out.println(4-3);
 		System.out.println(4*3);
 		System.out.println(16/4);
+		System.out.println(8/3.0);
+		System.out.println(16/4.0);
+		System.out.println(16/4F);
+		// f oder D verwenden
+		System.out.println(9/3F);
+		System.out.println(9D/3);
+		//
+		System.out.println(1+1+"Ergebnis");
+		System.out.println("3+3="+3+3);
+		//
+		System.out.println((1+1)+"Ergebnis");
+		System.out.println("3+3="+(3+3));
+		
+		
 	}
 
 }
