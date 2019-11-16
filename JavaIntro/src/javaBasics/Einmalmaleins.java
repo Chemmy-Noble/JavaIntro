5a483357cc50c280867fa75d263a280eac68adf5
Megaport <Megaport@192.168.0.22>
2019-11-16 15:51:04 +0100

    aight did it
    Signed-off-by: Megaport <Megaport@192.168.0.22>

---
+++ JavaIntro/src/javaBasics/Einmalmaleins.java
@@ -0,0 +1,15 @@
+package javaBasics;
+
+public class Einmalmaleins {
+
+	public static void main(String[] args) {
+		// FOR  Schleife
+		for (int i = 0; i < 11; i = i+1)   {
+			for (int j = 0; j < 11; j = j+1)   {
+				System.out.println(j + "x" + i +"="+ (i*j));
+			}
+		}
+
+	}
+
+}
