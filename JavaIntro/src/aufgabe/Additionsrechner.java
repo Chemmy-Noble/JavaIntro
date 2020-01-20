b035e0d00d43aaa5aafdf1cdc8105e6aef62e9af
chiemerie.noble <chiemerie.noble@AMS-GYM-EDV1-05.gym.ams.local>
2020-01-20 18:10:34 +0100

    j

--- JavaIntro/src/aufgabe/Additionsrechner.java
+++ JavaIntro/src/aufgabe/Additionsrechner.java
@@ -4,16 +4,33 @@ import java.util.Scanner;
 
 public class Additionsrechner {
 
-public static void main(String[] args) {
-int L,F,B;
-Scanner eingabewert = new Scanner(System.in);
+	public static void main(String[] args) {
+
+		Scanner eingabewert = new Scanner(System.in);
 //
-        System.out.print("Geben Sie die Laenge L ein (in cm): ");
-        L = eingabewert.nextInt();
-        System.out.print("Geben Sie die Breite B ein (in cm): ");
-        B = eingabewert.nextInt();
-        F = L * B;
-        System.out.println("Die Flaeche des Rechtecks ist: " + F + " cm^2");
-}
+		float a, b, c;
+		String operator;
+		System.out.print("Erste Zahl:");
+		a = eingabewert.nextInt();
+		System.out.print("Zweite Zahl:");
+		b = eingabewert.nextInt();
+		System.out.print("Operator:");
+		operator = eingabewert.next();
+		if (operator.equals("+")) {
+			c = a + b;
+			System.out.print("Ergebnis" + c);
+		} else if (operator.equals("*")) {
+			c = a * b;
+			System.out.print("Ergebnis" + c);
+		} else if (operator.equals("-")) {
+			c = a - b;
+			System.out.print("Ergebnis" + c);
+		} else if (operator.equals("/")) {
+			c = a / b;
+			System.out.print("Ergebnis" + c);
+		} else {System.out.print("Syntax Error");}; 
+		;
+
+	}
 
 }
