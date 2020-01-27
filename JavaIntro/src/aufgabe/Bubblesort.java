3aa70d5e53d2f24cc6ef454717c3f015da090c65
chiemerie.noble <chiemerie.noble@AMS-GYM-EDV1-05.gym.ams.local>
2020-01-27 18:09:58 +0100

    hi

--- JavaIntro/src/aufgabe/Bubblesort.java
+++ JavaIntro/src/aufgabe/Bubblesort.java
@@ -3,12 +3,20 @@ package aufgabe;
 public class Bubblesort {
 
 	public static void main(String[] args) {
-		int a,b;
-		int zahlen [] = {9,1,5,7,4,6,3,2,8,0};
-		for (a=0; a < zahlen.length; a++); {
-		
+		// TODO Auto-generated method stub
+		int[] zahlen = { 9, 1, 5, 7, 4, 6, 3, 2, 8, 0 };
+		int k;
+		for (int i = 1; i < zahlen.length; i++) {
+			for (int j = i; j > 0; j--) {
+				if (zahlen[j] < zahlen[j - 1]) {
+					k = zahlen[j];
+					zahlen[j] = zahlen[j - 1];
+					zahlen[j - 1] = k;
+				}
+			}
 		}
-		
+		for (int i = 0; i < zahlen.length; i++) {
+			System.out.println(zahlen[i]);
+		}
 	}
-
 }
