c2a4ae7c3e00e93dad70c19aacdfae33747d5447
chiemerie.noble <chiemerie.noble@AMS-GYM-EDV1-05.gym.ams.local>
2020-02-17 18:50:27 +0100

    ok

--- JavaIntro/src/aufgabe/Dowhileschleife.java
+++ JavaIntro/src/aufgabe/Dowhileschleife.java
@@ -1,9 +1,30 @@
 package aufgabe;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Dowhileschleife {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner eingabe = new Scanner(System.in);
+		String line = "";
+		ArrayList<String> name = new Arraylist<String>();
+		while (! line.contentEquals("y")) {
+			line = eingabe.nextLine();
+			name.add(line);
+		}
+		for (i = 1; i < name.size(); i++) {
+			for (j = 0; j > 0; j--) {
+				if (name.get(j).compareToIgnoreCase(name.get(j + 1)) > 0) {
+					l = name.get(j);
+					name.set(j, name.get(j + 1));
+					name.set(j + 1, l);
+					System.out.println(name);
+
+				}
+			}
+		}
+
 
 	}
 
